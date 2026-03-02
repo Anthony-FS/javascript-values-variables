1c49e390bd0961614a696e3b8e3a5cf0af4a9407
AnthonyFS <anthonyfungesmith@gmail.com>
2026-03-02 20:05:07 +0700

    completed assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,22 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let firstName = "Karter";
+let lastName = "Anderson";
+let userAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netFoodPrice = 5000;
+const totalOpenRestaurants = 2500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+userAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+//Cannot assign a new value to a constant variable
+//netFoodPrice = 5000;
+//totalOpenRestaurants = 2500;
+
+console.log(userAddress)
+console.log(netFoodPrice)
+console.log(totalOpenRestaurants)  
